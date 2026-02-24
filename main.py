81c828470bc1d97032362d51319bc2a5ed16945a
ferdejnavarro <ferdejnavarro@gmail.com>
2026-02-23 20:49:58 -0700

    Implement API scraping

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 import pandas as pd
+import requests
 
 app = Flask(__name__)
 
-df = pd.read_csv('dictionary.csv')
+# df = pd.read_csv('dictionary.csv')
+API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
 @app.route("/")
 def home():
@@ -11,10 +13,17 @@ def home():
 
 
 @app.route("/api/v1/<word>/")
-def dictionary(word):
-    definition = df.loc[df['word'] == word]['definition'].squeeze()
+# def dictionary(word):
+#     definition = df.loc[df['word'] == word]['definition'].squeeze()
+#     dictionary = {"definition": definition,
+#             "word": word}
+#     return dictionary
+def api(word):
+    response = requests.get(API_URL + word)
+    data = response.json()
+    definition = data[0]["meanings"][0]["definitions"][0]["definition"]
     dictionary = {"definition": definition,
-            "word": word}
+                 "word": word}
     return dictionary
 
 
